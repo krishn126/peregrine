bd00c8070949ee9bb7d5fb5a05c977edfef38d45
Krish Nanavati <knanavati126@gmail.com>
2025-03-06 13:41:11 +0000

    update the training thing

--- peregrine/snpe_custom.py
+++ peregrine/snpe_custom.py
@@ -390,11 +390,11 @@ if __name__ == "__main__":
                 ) as pbar: # Fancy tqdm loading bar for printing the training status
                         #iterate through the training examples
                     for i in range(len(training_example[0])):
-                        theta = theta[i,:].unsqueeze(0)
-                        x = training_example[i,:,:].unsqueeze(0)
-                        print(x.shape)
-                        print(theta.shape)
-                        loss = density_estimator.loss(theta, x).mean() # compute loss on batch
+                        theta_train = theta[i,:].unsqueeze(0)
+                        x_train = training_example[i,:,:].unsqueeze(0)
+                        print(x_train.shape)
+                        print(theta_train.shape)
+                        loss = density_estimator.loss(theta_train, x_train).mean() # compute loss on batch
                         optimizer.zero_grad() # zero the optimiser
                         loss.backward() # compute the gradients
                         optimizer.step() # take a step given these gradients
