c97c23705594bae261d232289d4a361298758e3c
Krish Nanavati <knanavati126@gmail.com>
2025-03-12 18:49:42 +0000

    remove obs from the training code

--- peregrine/snpe_custom.py
+++ peregrine/snpe_custom.py
@@ -334,21 +334,6 @@ if __name__ == "__main__":
                 d = torch.cat(d, dim=2)
 
                 return d
-            
-            
-            obs = (
-                    {key: torch.tensor(obs[key]) for key in ["d_t", "d_f", "d_f_w", "n_t", "n_f", "n_f_w"]}
-                )
-
-            obs["d_t"] = pad_to_length(obs["d_t"], 6, 0) 
-            obs["n_t"] = pad_to_length(obs["n_t"], 6, 0) 
-            obs["d_f"] = pad_to_width(obs["d_f"], 8192, 1) 
-            obs["d_f_w"] = pad_to_width(obs["d_f_w"], 8192, 1)
-            obs["n_f"] = pad_to_width(obs["n_f"], 8192, 1)
-            obs["n_f_w"] = pad_to_width(obs["n_f_w"], 8192, 1) 
-
-            obs = [obs[key] for key in ["d_t", "d_f", "d_f_w", "n_t", "n_f", "n_f_w"]]
-            obs = torch.cat(obs, dim=1)
 
             num_epochs = conf["hparams"]["max_epochs"]
             optimizer = AdamW(density_estimator.parameters(), lr=1e-3) # initialise pytorch optimiser
