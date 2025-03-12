1539b6095bd6244cadbe30a13526bbce0131bbf1
Krish Nanavati <knanavati126@gmail.com>
2025-03-12 23:38:29 +0000

    change to same style of figure as above

--- peregrine/load_posterior.py
+++ peregrine/load_posterior.py
@@ -271,12 +271,11 @@ if __name__ == "__main__":
     loaded_posterior = torch.load('/data/kn405/Code/peregrine_snpe/peregrine/peregrine/posterior.pt')
     posterior_samples = loaded_posterior.sample_batched(torch.Size([5000]), x=obs)
     # Plot posterior
-    for i in range(15):
-        plt.figure(figsize=(8, 5))
-        plt.hist(posterior_samples[:,0,i].numpy(), bins=30, density=True, alpha=0.7, label="Posterior samples")
-        plt.axvline(x=true_params[i], linestyle='--', label="True parameter value")
-        plt.xlabel(f"{order[i]}")
-        plt.ylabel("Density")
-        plt.title(f"Posterior Distribution for the parameter {order[i]}")
+    plt.figure(figsize=(15, 8))
+    for idx in range(15):
+        ax = plt.subplot(5, 3, idx + 1)
+        plt.hist(posterior_samples[:,0,idx].numpy(), bins=30, density=True, alpha=0.7, label="Posterior samples")
+        plt.axvline(x=true_params[idx], linestyle='--', label="True parameter value")
         plt.legend()
-        plt.savefig(f"/data/kn405/Code/peregrine_snpe/peregrine/posterior_plots/posterior_for_{order[i]}.png", dpi=300, bbox_inches='tight')
+        # plt.savefig(f"/data/kn405/Code/peregrine_snpe/peregrine/posterior_plots/posterior_for_{order[i]}.png", dpi=300, bbox_inches='tight')
+    plt.show()
