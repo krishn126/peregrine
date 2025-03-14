f8105887c0ceb44cf482265f5cdf1e6414627dc9
Krish Nanavati <knanavati126@gmail.com>
2025-03-14 12:22:18 +0000

    corner plot

--- peregrine/load_posterior.py
+++ peregrine/load_posterior.py
@@ -292,14 +292,14 @@ if __name__ == "__main__":
     lrs = pd.read_pickle('/data/kn405/Code/peregrine_snpe/peregrine/peregrine/logratios_R1')
 
     plt.figure(figsize=(15, 8))
-    for idx in range(15):
-        ax = plt.subplot(5, 3, idx + 1)
-        plt.hist(posterior_samples[:,0,idx].numpy(), range=ranges[idx], bins=100, density=True, alpha=0.7)
-        logratios = lrs.logratios[:, idx]
-        params = lrs.params[:, idx, 0]
-        plt.hist(params, weights=np.exp(logratios.numpy()), bins=100)
-        plt.axvline(x=true_params[idx], linestyle='--')
-        plt.legend()
-        # figure = corner.corner(posterior_samples[:,0,:].numpy())
+    # for idx in range(15):
+    #     ax = plt.subplot(5, 3, idx + 1)
+    #     plt.hist(posterior_samples[:,0,idx].numpy(), range=ranges[idx], bins=100, density=True, alpha=0.7)
+    #     logratios = lrs.logratios[:, idx]
+    #     params = lrs.params[:, idx, 0]
+    #     plt.hist(params, weights=np.exp(logratios.numpy()), bins=100)
+    #     plt.axvline(x=true_params[idx], linestyle='--')
+    #     plt.legend()
+    figure = corner.corner(posterior_samples[:,0,:].numpy())
         #corner plot of posteriors
     plt.savefig(f"/data/kn405/Code/peregrine_snpe/peregrine/posterior_plots/all_posteriors.png", dpi=300, bbox_inches='tight')
