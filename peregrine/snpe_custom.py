44c73a8cad73f5963ba00d12a8d3e27614fe143b
krishn126 <knanavai126@gmail.com>
2025-03-22 12:26:31 +0000

    updates to snpe_custom

--- peregrine/snpe_custom.py
+++ peregrine/snpe_custom.py
@@ -404,7 +404,7 @@ if __name__ == "__main__":
 
             density_estimator=density_estimator.to('cpu')
             posterior = DirectPosterior(density_estimator, joint_prior)
-            torch.save(density_estimator, "/data/kn405/Code/peregrine_snpe/peregrine/peregrine/density_estimator.pt")
-            torch.save(posterior, "/data/kn405/Code/peregrine_snpe/peregrine/peregrine/posterior.pt")
+            torch.save(density_estimator, "/data/kn405/Code/peregrine/peregrine/density_estimator.pt")
+            torch.save(posterior, "/data/kn405/Code/peregrine/peregrine/posterior.pt")
              
                 
