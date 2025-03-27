dbb9bd6deae6ff2cdfd2f8dd778cc1e6c8c5c158
Krish Nanavati <knanavati126@gmail.com>
2025-03-27 13:19:17 +0000

    final loss simulation experiment

--- peregrine/loss_simulation_experiment.py
+++ peregrine/loss_simulation_experiment.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
 # Sample data
-x = np.array([134272, 26752, 13312, 1280, 128])  # simulation number variables
-y = np.array([-4.79168, -2.19829, -0.26212, 6.31091, 8.74037])  # val loss variables
+x = np.array([134272, 26752, 13312, 6656, 2560, 1280, 128])  # simulation number variables
+y = np.array([-4.79168, -2.19829, -0.26212, 0.91438, 5.24674, 6.31091, 8.74037])  # val loss variables
 
 approx_crb = 52.545037343796665
 y = y + approx_crb  # Shift the validation loss to be positive
@@ -21,13 +21,13 @@ y_fit = np.exp(intercept) * x_fit**slope  # Convert back from log space
 
 # Plot log-log graph
 plt.figure(figsize=(8, 6))
-plt.loglog(x, y, 'o', label="Data")  # Plot original data
-plt.loglog(x_fit, y_fit, 'r-', label=f"Best Fit: y = {np.exp(intercept):.2f}x^{slope:.2f}")  # Best fit line
+plt.loglog(x, y, 'o', label="Val Loss")  # Plot original data
+plt.loglog(x_fit, y_fit, 'r--', label=f"Best Fit: y = {np.exp(intercept):.2f}x^{slope:.3f}")  # Best fit line
 
 # Labels and title
-plt.xlabel('Number of training simulations')
-plt.ylabel('Shifted Validation Loss (L - L_{CR})')
-plt.title('How Validation Loss changes with number of Training Simulations')
+plt.xlabel('Number of Training Simulations')
+plt.ylabel('Shifted Validation Loss (L - L_CR)')
+plt.title('How Validation Loss Changes with Number of Training Simulations')
 plt.legend()
 
 # Show the plot
