b51efa1d753baa28e6ee408a73e8a7e8b37262e9
Krish Nanavati <knanavati126@gmail.com>
2025-04-01 00:28:53 +0100

    try this instead

--- peregrine/load_posterior.py
+++ peregrine/load_posterior.py
@@ -488,6 +488,7 @@ if __name__ == "__main__":
     # credibility_levels, empirical_coverages = compute_coverage_per_param(1000, true_params)
     # plot_coverage(credibility_levels, empirical_coverages)
 
+    obs = obs.unsqueeze(0)
     expected_coverage, ideal_coverage = run_tarp(
     true_params,
     obs,
