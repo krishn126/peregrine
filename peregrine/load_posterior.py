26b292dac36e035657d0f5415c0cde40a7f80394
krishn126 <knanavai126@gmail.com>
2025-04-27 20:24:46 +0100

    updated proposal samples

--- peregrine/load_posterior.py
+++ peregrine/load_posterior.py
@@ -272,7 +272,7 @@ if __name__ == "__main__":
 
     #turn true_params into [1,15] torch tensor
     true_params = torch.tensor([true_params])
-    round_id = 3
+    round_id = 4
     
     def pad_to_width(t, target_width, i):
         current_width = t.shape[i]
