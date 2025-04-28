63c088f7c2e29e99bab57d5d0e389d2a0e58d9d4
krishn126 <knanavai126@gmail.com>
2025-04-29 00:55:29 +0100

    corner plot

--- peregrine/load_posterior.py
+++ peregrine/load_posterior.py
@@ -272,7 +272,7 @@ if __name__ == "__main__":
 
     #turn true_params into [1,15] torch tensor
     true_params = torch.tensor([true_params])
-    round_id = 2
+    round_id = 4
     
     def pad_to_width(t, target_width, i):
         current_width = t.shape[i]
@@ -464,8 +464,8 @@ if __name__ == "__main__":
         return fig
     
     # #Save Plots
-    fig = plot_posterior_npe_tmnre()
-    plt.savefig(f"/data/kn405/Code/peregrine/posterior_plots/round_{round_id}_vs_tmnre_{round_id}.png", dpi=300, bbox_inches='tight')
+    fig = corner_plot()
+    plt.savefig(f"/data/kn405/Code/peregrine/posterior_plots/round_{round_id}_vs_dynesty).png", dpi=300, bbox_inches='tight')
 
     #JS Divergence Calculations 
     def js_div_calcs():
