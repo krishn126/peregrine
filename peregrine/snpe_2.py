ec4ab8ae795399c17d3e54ade25d25bd091841b9
krishn126 <knanavai126@gmail.com>
2025-04-30 22:14:39 +0100

    change patience

--- peregrine/snpe_2.py
+++ peregrine/snpe_2.py
@@ -402,7 +402,7 @@ if __name__ == "__main__":
         num_val_batches = sum(1 for _ in val_data)
         best_validation_loss = float("inf")
         no_improvement_count = 0
-        patience = 20
+        patience = 8
 
         limit = int(num_train_batches)
         optimizer = AdamW(density_estimator.parameters(), lr=1e-3) 
